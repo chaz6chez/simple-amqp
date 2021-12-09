50bbd9bf37288b62e380c14f24f609537cf9eaf3
chaz6chez <250220719@qq.com>
2021-12-10 00:44:20 +0800

    update

--- src/AbstractConsumer.php
+++ src/AbstractConsumer.php
@@ -25,26 +25,13 @@ abstract class AbstractConsumer extends AbstractProcess {
         self::$_debug = $debug;
     }
 
-    protected function _dump($data){
-        if (self::$_debug) {
-            self::safeEcho(json_encode($data,JSON_UNESCAPED_UNICODE));
-        }else{
-            self::log(json_encode($data,JSON_UNESCAPED_UNICODE));
-        }
-    }
-
-    /**
-     * init Consumer and Builder
-     */
-    abstract protected function _init() : void;
-
     public function __invoke(): AbstractProcess
     {
         $this->_init();
         if(self::check() === false){
             self::check(true);
             $this->_dump('> Detecting rabbitmq connection ...');
-            if(!$this->_builder->consumer()->checker()){
+            if(!$this->_builder()->consumer()->checker()){
                 $this->_dump('? ' . get_called_class());
                 $this->_dump('? ' . $this->_builder->consumer()->getError()->getMessage());
                 $this->_dump('> Rabbitmq connection failed ...');
@@ -58,17 +45,42 @@ abstract class AbstractConsumer extends AbstractProcess {
     public function onStart(...$param): void
     {
         $this->_init();
-        $this->_builder->consume();
+        $this->_builder()->consume();
     }
 
     public function onReload(...$param): void
     {
-        $this->_builder->consumer()->reconnect();
+        $this->_builder()->consumer()->reconnect();
     }
 
     public function onStop(...$param): void
     {
-        $this->_builder->consumer()->close();
+        $this->_builder()->consumer()->close();
         $this->_builder = null;
     }
+
+    /**
+     * @param $data
+     */
+    protected function _dump($data){
+        if (self::$_debug) {
+            self::safeEcho(json_encode($data,JSON_UNESCAPED_UNICODE));
+        }else{
+            self::log(json_encode($data,JSON_UNESCAPED_UNICODE));
+        }
+    }
+
+    /**
+     * @return Builder
+     */
+    protected function _builder() : Builder
+    {
+        return $this->_builder;
+    }
+
+    /**
+     * init Builder、Logger and so on
+     */
+    abstract protected function _init() : void;
+
 }
