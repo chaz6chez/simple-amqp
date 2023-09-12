e046136785c164905eddd8ecdc6aa186e45c86ee
sunsgne <51745500+sunsgneayo@users.noreply.github.com>
2023-09-12 15:15:33 +0800

    Update SyncClient.php

--- src/Client/SyncClient.php
+++ src/Client/SyncClient.php
@@ -4,12 +4,55 @@ declare(strict_types=1);
 namespace SimpleAmqp\Client;
 
 use Bunny\Client;
+use Bunny\ClientStateEnum;
+use Bunny\Protocol\HeartbeatFrame;
+use Workerman\Timer;
 
 class SyncClient extends Client {
+
+    protected ?int $heartbeatTimer = null;
+
     public function __destruct()
     {
         try {
+            if($this->heartbeatTimer){
+                Timer::del($this->heartbeatTimer);
+            }
             parent::__destruct();
         }catch (\Throwable $throwable){}
+    }
+
+    /**
+     * @return SyncClient
+     * @throws \Exception
+     * @Time 2023/9/12 14:37
+     * @author sunsgne
+     */
+    public function connect()
+    {
+        $result = parent::connect();
+        /** 连接成功，开启心跳 */
+        if ($result->state === ClientStateEnum::CONNECTED){
+            $this->heartbeatTimer = Timer::add($this->options['heartbeat'] ?? 60, [$this, 'onHeartbeat']);
+        }
+        return $result;
+    }
+
+
+    /**
+     * Callback when heartbeat timer timed out.
+     * @return void
+     * @Time 2023/9/12 14:22
+     * @author sunsgne
+     */
+    public function onHeartbeat()
+    {
+        $this->writer->appendFrame(new HeartbeatFrame(), $this->writeBuffer);
+        $this->flushWriteBuffer();
+
+        if (is_callable($this->options['heartbeat_callback'] ?? null)) {
+            $heartbeat_status = $this->state === ClientStateEnum::CONNECTED ? true : false;
+            $this->options['heartbeat_callback']($this , $heartbeat_status);
+        }
     }
 }
